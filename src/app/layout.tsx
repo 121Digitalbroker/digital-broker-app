c6d7ec68e8f8390acfb116a7127c1e341cbe5b62
Dhruv <sdhruv640@gmail.com>
2026-05-03 14:53:06 +0530

    timer page SHow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,22 @@ export default function RootLayout({
             })(window,document,'script','dataLayer','GTM-P4N8R3K5');
           `}
         </Script>
+
+        {/* Meta Pixel Code */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '2019585948939444');
+            fbq('track', 'PageView');
+          `}
+        </Script>
       </head>
       <body className={`${inter.variable} antialiased`}>
         <ClerkProvider>
@@ -65,6 +81,16 @@ export default function RootLayout({
               height="0"
               width="0"
               style={{ display: "none", visibility: "hidden" }}
+            />
+          </noscript>
+
+          {/* Meta Pixel (noscript) */}
+          <noscript>
+            <img
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=2019585948939444&ev=PageView&noscript=1"
             />
           </noscript>
 
